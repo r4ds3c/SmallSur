4780fdcda29f7ebb6d9a1cc94f74d21783a8ed46
0xr4ds3c <323016+r4ds3c@users.noreply.github.com>
2025-10-25 00:46:19 +0200

    Remove Xfce4 panel theme application commands
    Removed Xfce4 panel theme application commands from the script.

--- install-arch.sh
+++ install-arch.sh
@@ -27,17 +27,6 @@ mkdir -p ~/.local/share/plank/themes/
 cp -rp WhiteSur-gtk-theme/src/other/plank/* ~/.local/share/plank/themes/
 cp -rp plank/mcOS-BS-iMacM1-Black/ ~/.local/share/plank/themes/
 
-#Xfce4-panel
-sudo killall xfce4-panel 
-mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp -rp xfce4-panel/xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
-
-#Applying theme 
-xfconf-query -c xsettings -p /Net/ThemeName -s "WhiteSur-dark" 
-#Icon theme
-xfconf-query -c xsettings -p /Net/IconThemeName -s 'WhiteSur-dark' 
-#Cursor theme
-xfconf-query -c xsettings -p /Gtk/CursorThemeName -s "WhiteSur Cursors" 
 
 echo "SmallSur installed"
 echo "Reboot You system"
